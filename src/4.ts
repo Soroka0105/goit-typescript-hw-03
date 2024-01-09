46b256bcf159b9e055c8144e49714e61ffca7252
Soroka0105 <soroka0105@gmail.com>
2024-01-09 14:03:36 +0200

    1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+    private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+    getSignature():number{
+return this.signature
+    }
+}
+
+class Person{
+    private key: Key;
+    constructor(key:Key){
+this.key = key
+    }
+    getKey():Key{
+return this.key
+    }
+}
+
+abstract class House{
+    protected key: Key;
+   constructor(key:Key){
+    this.key = key
+   }
+   abstract openDoor(key:Key): void;
+}
+
+class MyHouse extends House{
+constructor(key:Key) {
+    super(key)
+}
+openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      console.log('Opened');
+    } else {
+      console.log('Invalid key');
+    }
+  }
+  comeIn(person){
+console.log(`Welcome ${person}`);
+  }
+}
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
